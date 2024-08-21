f812960036f2a66fec4266c2641ddcdc56e9c107
Mert <cm.sapci@gmail.com>
2024-08-21 16:32:43 +0200

    Entfernen angepasst

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ print(Liste)
 
 einkaufsliste = []
 
-
 while True:
     user_input = input('Möchtest du was hinzufügen, entfernen oder anzeigen lassen? : ')
 
@@ -17,19 +16,19 @@ while True:
 
 
     elif user_input.lower() == 'entfernen':
-        item = input('was möchtest du entfernen ? ')
-        if item in einkaufsliste:
-            einkaufsliste.remove(input())
-            print('Artikel wurde gelöscht! ')
+        if not einkaufsliste:
+            print('Die Liste ist Leer da gibt es nichts zu entfernen.')
         else:
-            print('Artikel gibt es nicht! ')
+            item = input('Was möchtest du entfernen ?')
+            if item in einkaufsliste:
+                einkaufsliste.remove(item)
+                print('Artikel wurde gelöscht ')
+
+
+
 
     elif user_input.lower() == 'anzeigen lassen':
         print('In deiner Einkaufsliste befindet sich :', einkaufsliste, )
     elif user_input.lower() == 'beenden':
-        break
+
         print('Das Programm ist zu Ende :) ')
-
-
-
-
